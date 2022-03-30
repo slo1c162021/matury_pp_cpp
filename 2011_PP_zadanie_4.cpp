55b6e9cec734ef0bc61055ce8dfac1e3912d5974
slo1c162021 <81370445+slo1c162021@users.noreply.github.com>
2022-03-30 07:16:28 +0200

    Add files via upload

---
+++ 2011_PP_zadanie_4.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace
+int main()
+{
+   int licznik = s;
+   string s;
+   ifstream f("hasla.txt");
+   while(f>>s){
+    if(s.size()% 2 == s)licznik++;
+
+   }
+
+
+}
